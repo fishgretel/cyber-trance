8784d9c7fd3a6f2d3c7f777d520988c4b787e253
Jorge Bucaran <j@bucaran.me>
2016-03-31 18:10:07 +0900

    Classical prompt inspired by hacking the kernel.

---
+++ README.md
@@ -0,0 +1,29 @@
+[![Slack Room][slack-badge]][slack-link]
+
+# Cyber
+
+Classical prompt inspired by hacking the kernel.
+
+![Cyber]
+
+## Install
+
+With [Fisherman]
+
+```fish
+fisher i cyber
+```
+
+## Terminal Settings
+
+* [Cyber.terminal]
+* [Cyber.itermcolors]
+* Font: 16pt Anonymous Pro Regular
+
+[slack-link]: https://fisherman-wharf.herokuapp.com/
+[slack-badge]: https://img.shields.io/badge/slack-join%20the%20chat-00B9FF.svg?style=flat-square
+
+[Fisherman]: https://github.com/fisherman/fisherman
+[Cyber.terminal]: https://github.com/fishery/cyber/raw/master/Cyber.terminal
+[Cyber.itermcolors]: https://github.com/fishery/cyber/raw/master/Cyber.itermcolors
+[Cyber]: https://cloud.githubusercontent.com/assets/8317250/14171008/9ebc42ea-f76b-11e5-9c00-b125e939247c.png
---
+++ functions/fish_right_prompt.fish
@@ -0,0 +1,32 @@
+function fish_right_prompt
+    set -l status_copy $status
+    set -l status_code $status_copy
+    set -l status_color 090
+
+    switch "$status_copy"
+        case "$__cyber_status_last" 0
+            set status_code
+    end
+
+    set -g __cyber_status_last $status_copy
+
+    if test "$status_copy" -ne 0
+        set status_color c00
+    else
+        set -e status_copy
+    end
+
+    if test ! -z "$status_code"
+        echo -sn (set_color $status_color) "($status_code)  " (set_color normal)
+    end
+
+    if test "$CMD_DURATION" -gt 100
+        set -l duration (echo $CMD_DURATION | humanize_duration)
+        echo -sn (set_color $status_color) "$duration  " (set_color normal)
+
+    else if set -l id (last_job_id)
+        echo -sn (set_color $status_color) "%$id  " (set_color normal)
+    end
+
+    echo -sn (set_color -b $status_color) " " (set_color normal)
+end
