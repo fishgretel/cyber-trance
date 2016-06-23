df3a7161f8dd3dd4f07c6135015a4dbdaa491588
Tom Hensel <github@jitter.eu>
2016-06-24 01:42:51 +0200

    have color scheme changed
    [skip ci]

--- functions/set_color_custom.fish
+++ functions/set_color_custom.fish
@@ -1,24 +1,49 @@
 function set_color_custom
+    set -U fish_color_autosuggestion        99f
+    set -U fish_color_command               99e
+    set -U fish_color_comment               55c
+    set -U fish_color_cwd                   bde
+    set -U fish_color_cwd_root              ede
+    set -U fish_color_end                   77c
+    set -U fish_color_error                 82e
+    set -U fish_color_escape                a7e
+    set -U fish_color_history_current       79d
+    set -U fish_color_match                 88e
     set -U fish_color_normal                normal
-    set -U fish_color_command               66e
-    set -U fish_color_param                 44a
-    set -U fish_color_redirection           66a
-    set -U fish_color_comment               66a
-    set -U fish_color_error                 a0f
-    set -U fish_color_escape                66e
-    set -U fish_color_operator              66e
-    set -U fish_color_end                   66e
-    set -U fish_color_quote                 44a
-    set -U fish_color_autosuggestion        77b
+    set -U fish_color_operator              88e
+    set -U fish_color_param                 77e
+    set -U fish_color_quote                 55c
+    set -U fish_color_redirection           e77
+    set -U fish_color_search_match          --background=005
+    set -U fish_color_selection             --background=306
     set -U fish_color_valid_path            --underline
-    set -U fish_color_cwd                   eef
-    set -U fish_color_cwd_root              eef
-    set -U fish_color_match                 eef
-    set -U fish_color_search_match          --background=002
-    set -U fish_color_selection             --background=33e
-    set -U fish_pager_color_prefix          44a
-    set -U fish_pager_color_completion      77d
-    set -U fish_pager_color_description     eef
-    set -U fish_pager_color_progress        66c
-    set -U fish_color_history_current       66c
+    set -U fish_pager_color_completion      66d
+    set -U fish_pager_color_description     55c
+    set -U fish_pager_color_prefix          55c
+    set -U fish_pager_color_progress        60e
 end
+
+# function set_color_custom
+#      set -U fish_color_normal                normal
+#      set -U fish_color_command               66e
+#      set -U fish_color_param                 44a
+#      set -U fish_color_redirection           66a
+#      set -U fish_color_comment               66a
+#      set -U fish_color_error                 a0f
+#      set -U fish_color_escape                66e
+#      set -U fish_color_operator              66e
+#      set -U fish_color_end                   66e
+#      set -U fish_color_quote                 44a
+#      set -U fish_color_autosuggestion        77b
+#      set -U fish_color_valid_path            --underline
+#      set -U fish_color_cwd                   eef
+#      set -U fish_color_cwd_root              eef
+#      set -U fish_color_match                 eef
+#      set -U fish_color_search_match          --background=002
+#      set -U fish_color_selection             --background=33e
+#      set -U fish_pager_color_prefix          44a
+#      set -U fish_pager_color_completion      77d
+#      set -U fish_pager_color_description     eef
+#      set -U fish_pager_color_progress        66c
+#      set -U fish_color_history_current       66c
+# end
